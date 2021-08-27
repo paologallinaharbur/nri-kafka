e417c892ba1bc4e68095f1596080cbfe5ad808e5
paologallinaharbur <paologallina1992@gmail.com>
2021-08-27 15:32:45 +0200

    debug

--- build/windows/fix_archives.sh
+++ build/windows/fix_archives.sh
@@ -27,7 +27,8 @@ find dist -regex ".*_dirty\.zip" | while read zip_dirty; do
 
   echo "===> Move files inside ${zip_file_name}"
   mv ${ZIP_CONTENT_PATH}/nri-${INTEGRATION}.exe "${AGENT_DIR_IN_ZIP_PATH}/bin"
-  mv ${ZIP_CONTENT_PATH}/${INTEGRATION}-win-definition.yml "${AGENT_DIR_IN_ZIP_PATH}"
+  mv ${ZIP_CONTENT_PATH}/${INTEGRATION}-win-definition.yml ${ZIP_CONTENT_PATH}/${INTEGRATION}-definition.yml
+  mv ${ZIP_CONTENT_PATH}/${INTEGRATION}-definition.yml "${AGENT_DIR_IN_ZIP_PATH}"
   mv ${ZIP_CONTENT_PATH}/${INTEGRATION}-win-config.yml.sample "${CONF_IN_ZIP_PATH}"
 
   echo "===> Creating zip ${ZIP_CLEAN}"
